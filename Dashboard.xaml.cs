7aa97d95330ec77398489e17c786e43ada37deb4
mohammedshady433 <mohammedshady433@gmail.com>
2024-11-27 02:34:19 +0200

    commit for pull test

--- Dashboard.xaml.cs
+++ Dashboard.xaml.cs
@@ -27,7 +27,7 @@ namespace E_Vita
 
         private void add_Patient(object sender, RoutedEventArgs e)
         {
-            NavigationService?.Navigate(new Add_Patient());
+            this.NavigationService.Navigate(new Doctor_Dashboard());
         }
     }
 }
