62e096cba8e8d3728381db23266675728581e239
mohammedshady433 <mohammedshady433@gmail.com>
2024-12-03 14:39:30 +0200

    the db commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using MySql.EntityFrameworkCore;
+
+namespace E_Vita
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Database=MyDatabase;User=root;Password=1234;",
+                new MySqlServerVersion(new Version(8, 0, 21)));
+        }
+    }
+}
---
+++ Models/Nurse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace E_Vita
+{
+    public class Nurse
+    {
+        public int Nurse_ID{get; set; }
+        public string User_name{get; set; }
+        public string Name{get; set; }
+        public string speciality {  get; set; }
+        public string pass {  get; set; }
+        [ForeignKey("Appointment")]
+        public int patient_ID { get; set; }
+        [ForeignKey("Appointment")]
+        public int Doctor_ID { get; set; }
+
+    }
+}
