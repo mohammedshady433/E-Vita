b8fe4f15c58e8770e538a3cd466eddb46d0b42ae
mohammedshady433 <mohammedshady433@gmail.com>
2024-12-16 13:43:24 +0200

    add_patient have been modified

--- Views/Add_Patient.xaml.cs
+++ Views/Add_Patient.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
+using System.Text.Json;
 namespace E_Vita
 {
     /// <summary>
@@ -23,6 +24,26 @@ namespace E_Vita
         public Add_Patient()
         {
             InitializeComponent();
+            LoadNationalities();
+        }
+        private void LoadNationalities()
+        {
+            // Path to the JSON file
+            string filePath = "D:\\NU\\fifth semester\\Clinical\\E-Vita\\E-Vita\\Assets\\Json_files\\nationalities-common.json";
+
+            // Read the JSON file
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Parse the JSON to extract the nationalities
+            var data = JsonSerializer.Deserialize<NationalityData>(jsonContent);
+
+            // Bind the nationalities to the ComboBox
+            NationalitiesComboBox.ItemsSource = data.Nationalities;
+        }
+        // Helper class to match the JSON structure
+        public class NationalityData
+        {
+            public List<string> Nationalities { get; set; }
         }
         private void Appointments_Click(object sender, RoutedEventArgs e)
         {
