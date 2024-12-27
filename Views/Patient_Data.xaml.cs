329a7df9a29bf0c8cd1e9f425844c43bf3c58e63
sandy <s.melad2265@nu.edu.eg>
2024-12-27 04:31:07 +0200

    4:30

--- Views/Patient_Data.xaml.cs
+++ Views/Patient_Data.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Azure;
+using E_Vita.Interfaces.Repository;
+using E_Vita.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +25,71 @@ namespace E_Vita
     /// </summary>
     public partial class Patient_Data : Page
     {
+        private readonly IRepository<Medical_Record> _search_for_patient;
+
         public Patient_Data()
         {
             InitializeComponent();
+            var services = ((App)Application.Current)._serviceProvider;
+            _search_for_patient = services.GetService<IRepository<Medical_Record>>() ?? throw new InvalidOperationException("Data helper service is not available");
+
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (search_by_id.CanGoBack)
+            {
+                search_by_id.GoBack();
+            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
         {
-                this.NavigationService.Navigate(new Finance());
+            if (search_by_id.CanGoForward)
+            {
+                search_by_id.GoForward();
+            }
         }
+
+        private async Task LoadMedicalRecordsAsync(int patientId)
+        {
+            // Retrieve medical records for the given patient ID
+            var medicalRecords = await _search_for_patient.MedicalRecords
+                .Where(m => m.Patient_ID == patientId)  // Filter by Patient_ID
+                .ToListAsync();
+
+            // If records are found, bind to the DataGrid
+            if (medicalRecords.Any())
+            {
+                PatientHistoryDataGrid.ItemsSource = medicalRecords.Select(record => new
+                {
+                    record.Date,
+                    record.Future_Plan,
+                    record.Disease,
+                    record.Medication,
+                    record.Surgery,
+                    record.Family_History,
+                    record.reason_for_visit
+                });
+            }
+            else
+            {
+                MessageBox.Show("No medical records found for this patient.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(SearchForPatientTextBox.Text, out int patientId))
+            {
+                await LoadMedicalRecordsAsync(patientId);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Patient ID.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+
     }
 }
+    
+
